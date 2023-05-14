50d0029eb909ffcb7eff5c6f70b153bebd6ac608
Mohammad Javadpur <132184890+mjavadpur@users.noreply.github.com>
2023-05-14 13:27:14 +0330

    Update FIDS.sql

--- FIDS.sql
+++ FIDS.sql
@@ -1,8 +1,15 @@
 
+
 select COUNT(*) as cancelled_cnt, scheduled_date
-       from [FIDS].dbo.FIDS where  status_ = 'Cancelled' group by scheduled_date order by cancelled_cnt desc
+       from [FIDS].dbo.FIDS 
+	   where  status_ = 'Cancelled' or replace(status_,' ','') = 'باطلشد' or replace(status_,' ','') = 'باطلشد' 
+	   group by scheduled_date 
+	   order by cancelled_cnt desc
 
 
 
-select sum(delay_) as delayAmount, dow
-       from [FIDS].dbo.FIDS  group by dow order by delayAmount desc
+
+select replace(dow,' ','') as day_name, sum(delay_) as delayAmount
+       from [FIDS].dbo.FIDS  
+	   group by day_name
+	   order by delayAmount desc
